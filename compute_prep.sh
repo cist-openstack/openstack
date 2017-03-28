8e3f9f2dbed0b1d92931b7f6a349b4cf76ca15e1
root <root@hv1.cist.pitt.edu>
2017-03-27 22:12:06 -0400

    fxes

--- compute_prep.sh
+++ compute_prep.sh
@@ -4,40 +4,41 @@ IP=`ip ad | grep 'inet ' | grep -v 127.0.0.1 | grep 136 | awk '{print $2}' | cut
 
 if [ "$IP" == "136.142.139.131" ]; then
  hostnamectl set-hostname compute1.cist.pitt.edu
-elif [ "$IP" == "136.142.139.132" ]; then
+elif [ "$IP" == "136.142.139.132" ]
  hostnamectl set-hostname compute5.cist.pitt.edu
-elif [ "$IP" == "136.142.139.136" ]; then
+elif [ "$IP" == "136.142.139.136" ]
  hostnamectl set-hostname compute2.cist.pitt.edu
-elif [ "$IP" == "136.142.139.138" ]; then
+elif [ "$IP" == "136.142.139.138" ]
  hostnamectl set-hostname compute3.cist.pitt.edu
-elif [ "$IP" == "136.142.139.139" ]; then
+elif [ "$IP" == "136.142.139.139" ]
  hostnamectl set-hostname compute4.cist.pitt.edu
-elif [ "$IP" == "136.142.139.140" ]; then
+elif [ "$IP" == "136.142.139.140" ]
  hostnamectl set-hostname compute6.cist.pitt.edu
-elif [ "$IP" == "136.142.139.141" ]; then
+elif [ "$IP" == "136.142.139.141" ]
  hostnamectl set-hostname compute7.cist.pitt.edu
-elif [ "$IP" == "136.142.139.142" ]; then
+elif [ "$IP" == "136.142.139.142" ]
  hostnamectl set-hostname compute8.cist.pitt.edu
-elif [ "$IP" == "136.142.139.143" ]; then
+elif [ "$IP" == "136.142.139.143" ]
  hostnamectl set-hostname compute9.cist.pitt.edu
-elif [ "$IP" == "136.142.139.144" ]; then
+elif [ "$IP" == "136.142.139.144" ]
  hostnamectl set-hostname compute10.cist.pitt.edu
-elif [ "$IP" == "136.142.139.146" ]; then
+elif [ "$IP" == "136.142.139.146" ]
  hostnamectl set-hostname compute11.cist.pitt.edu
-elif [ "$IP" == "136.142.139.147" ]; then
+elif [ "$IP" == "136.142.139.147" ]
  hostnamectl set-hostname compute12.cist.pitt.edu
-elif [ "$IP" == "136.142.139.148" ]; then
+elif [ "$IP" == "136.142.139.148" ]
  hostnamectl set-hostname compute13.cist.pitt.edu
-elif [ "$IP" == "136.142.139.149" ]; then
+elif [ "$IP" == "136.142.139.149" ]
  hostnamectl set-hostname compute14.cist.pitt.edu
-elif [ "$IP" == "136.142.139.150" ]; then
+elif [ "$IP" == "136.142.139.150" ]
  hostnamectl set-hostname compute15.cist.pitt.edu
-elif [ "$IP" == "136.142.139.151" ]; then
+elif [ "$IP" == "136.142.139.151" ]
  hostnamectl set-hostname compute16.cist.pitt.edu
-elif [ "$IP" == "192.168.100.218" ]; then
+elif [ "$IP" == "192.168.100.218" ]
  hostnamectl set-hostname derp.cist.pitt.edu
 else
  hostnamectl set-hostname isuckatipaddresses.cist.pitt.edu
+fi
 
 yum -y install epel-release;
 yum -y install wget mlocate vim chrony centos-release-openstack-liberty git;
