cdcbfd4d6b7ff77f9320378f5b6c079483c7f7d7
root <root@newton-controller.cist.pitt.edu>
2017-06-18 17:40:34 -0400

    Updated grub file

--- image_automation/centos7/centos_7.sh
+++ image_automation/centos7/centos_7.sh
@@ -17,38 +17,38 @@ fi
 
 # Set up GUI
 if [ $GUI == "y" ]; then
-    yum -y group install "X Window System" "GNOME"
+    /usr/bin/yum -y group install "X Window System" "GNOME"
     systemctl set-default graphical.target    
     if [ "$VNC" == "y" ]; then
-        yum -y install tiger-vnc tigervnc tigervnc-server
-        cp /usr/lib/systemd/system/vncserver\@.service /etc/systemd/system/vncserver@.service
+        /usr/bin/yum -y install tiger-vnc tigervnc tigervnc-server
+        /usr/bin/cp /usr/lib/systemd/system/vncserver\@.service /etc/systemd/system/vncserver@.service
         sed -ie "s/<USER>/$VNCUSER/g" /etc/systemd/system/vncserver\@.service
         useradd $VNCUSER
     fi
 fi
 
 # Install epel repository
-yum -y install epel-release.noarch
+/usr/bin/yum -y install epel-release.noarch
 
 # Get cool packages
-yum -y install mlocate bind-utils vim samba-client httpd nfs-utils net-tools
-yum -y install gdisk cifs-utils at wget lvm2 star git acpid  tmux chrony telnet ansible nrpe fping
-yum -y install firewalld fail2ban python2-pip.noarch cloud-init java
+/usr/bin/yum -y install mlocate bind-utils vim samba-client httpd nfs-utils net-tools
+/usr/bin/yum -y install gdisk cifs-utils at /usr/bin/wget lvm2 star git acpid  tmux chrony telnet ansible nrpe fping
+/usr/bin/yum -y install firewalld fail2ban python2-pip.noarch cloud-init java
    
 # Download and install telegraf for Grafana metric collection
-wget https://dl.influxdata.com/telegraf/releases/telegraf-1.2.1.x86_64.rpm
-sudo yum -y localinstall telegraf-1.2.1.x86_64.rpm
+/usr/bin/wget https://dl.influxdata.com/telegraf/releases/telegraf-1.2.1.x86_64.rpm
+sudo /usr/bin/yum -y localinstall telegraf-1.2.1.x86_64.rpm
 
 # Upgrade system
-yum -y upgrade;
+/usr/bin/yum -y upgrade;
 
 # Configure telegraf
-git clone https://github.com/cist-openstack/openstack.git
+/usr/bin/git clone https://github.com/cist-openstack/openstack.git
 unalias cp
-cp openstack/telegraf/hangar-snapstack-lab/telegraf.conf /etc/telegraf/telegraf.conf
-cp openstack/image_automation/centos7/sudoers /etc/sudoers
-cp openstack/image_automation/centos7/cloud.cfg /etc/cloud/cloud.cfg
-
+/usr/bin/cp openstack/telegraf/hangar-snapstack-lab/telegraf.conf /etc/telegraf/telegraf.conf
+/usr/bin/cp openstack/image_automation/centos7/sudoers /etc/sudoers
+/usr/bin/cp openstack/image_automation/centos7/cloud.cfg /etc/cloud/cloud.cfg
+/usr/bin/cp openstack/image_automation/centos7/grub /etc/default/grub
 # Start chrony, telegraf, firewall, cloud-init
 systemctl start chronyd
 systemctl enable chronyd
